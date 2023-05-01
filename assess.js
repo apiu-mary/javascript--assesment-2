610a7547f063046388a8d70a47c261d05a5e29e9
apiuuagou@gmail.com <apiuagou@gmail.com>
2023-05-01 08:30:16 +0300

    first commit

---
+++ assess.js
@@ -0,0 +1,18 @@
+function getAvailablebooks(arr){
+
+
+}
+let book=["The Catcher in the Rye","Great Expectations","Crime and Punishment","The Odyssey"]
+console.log(arr(book))
+// Create a function getBooksByAuthor that takes an author's name as an argument and
+// returns an array of all books by that author.
+
+
+const books={
+    ["Catcher in the Rye,JD Salinger"]
+    ["Great Expectations,Charles Dickens"]
+    ["Crime and Punishment,Fyodor Dostoevsky"]
+    ["The Great Gatsby,F. Scott Fitzgerald,"]
+    ["The Odyssey,Homer"]
+
+}
