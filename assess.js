ccbefcc5a4c1d315ff52f951b312a4bba367531d
apiuuagou@gmail.com <apiuagou@gmail.com>
2023-05-01 09:05:28 +0300

    second commit

--- assess.js
+++ assess.js
@@ -1,3 +1,27 @@
+
+   const person1={
+    name:"JD Salinger",
+    book:"The Catcher",
+
+   } 
+   const person2={
+    name:"Charles Dickens",
+    book:"Great Expectations"
+   }
+   const person3={
+    name:"Fyodor Dostoevsky",
+    book:"Crime and Punishment"
+   }
+   const person4={
+    name:"Scott Fitzgerald",
+    book:"he Great Gatsby"
+   }
+//    2. Create a function getBooksByAuthor that takes an author's name as an argument and
+// returns an array of all books by that author.
+function getBooksByAuthor(name){
+    
+}
+getBooksByAuthor()
 function getAvailablebooks(arr){
 
 
@@ -16,3 +40,23 @@ const books={
     ["The Odyssey,Homer"]
 
 }
+console.log(books)
+// Create a function addNewBook that takes a book object as an argument and adds it
+// to the library, ensuring that the new book has all required properties (title, author,
+// publicationYear, and isAvailable).
+
+        
+        
+    
+
+
+
+// 2. Create a function getBooksByAuthor that takes an author's name as an argument and
+// returns an array of all books by that author.
+// 3. Create a function addNewBook that takes a book object as an argument and adds it
+// to the library, ensuring that the new book has all required properties (title, author,
+// publicationYear, and isAvailable).
+
+
+
+
